6f60ca6b7c6fada74a12d23f54bf673666e6bba4
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-09-23 16:30:28 +0100

    delete Eric Cartman

--- marvel.sql
+++ marvel.sql
@@ -63,8 +63,10 @@ INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:0
 
 -- DELETE FROM movies WHERE title = 'Batman Begins';
 
-INSERT INTO people (name) VALUES
-('Bart Simpson');
+-- INSERT INTO people (name) VALUES
+-- ('Bart Simpson');
+
+DELETE FROM people WHERE name = 'Eric Cartman';
 
 SELECT * FROM movies;
 SELECT * FROM people;
